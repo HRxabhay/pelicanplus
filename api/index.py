64a0f7d05e981c722d33adb33c4dc8797ddaf59d
HRxabhay <88188329+HRxabhay@users.noreply.github.com>
2023-09-17 19:05:30 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,16 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/api/python")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    # Open the pseudoindex.txt file for reading
+    with open("pseudoindex.txt", "r") as file:
+        # Read the contents of the file
+        file_contents = file.read()
+
+    # Return the contents of the file as the response
+    return file_contents
+
+if __name__ == "__main__":
+    app.run()
