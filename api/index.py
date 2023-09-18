aa2fc4f8fbc572cf88072c00c03354d8f59b8197
HRxabhay <88188329+HRxabhay@users.noreply.github.com>
2023-09-18 11:52:11 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,13 +4,7 @@ app = Flask(__name__)
  
 @app.route("/api/python")
 def hello_world():
-    # Open the pseudoindex.txt file for reading
-    with open("pseudoindex.txt", "r") as file:
-        # Read the contents of the file
-        file_contents = file.read()
-
-    # Return the contents of the file as the response
-    return file_contents
+ return "mscmsmc"
 
 if __name__ == "__main__":
     app.run()
